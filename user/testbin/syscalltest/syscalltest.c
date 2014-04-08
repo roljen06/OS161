4300f3383590d3301e1a22d45029e5fc6069597d
roljen06 <jennifer.l.rollet@gmail.com>
2014-04-08 14:00:58 -0700

    syscalltest.c

--- user/testbin/syscalltest/syscalltest.c
+++ user/testbin/syscalltest/syscalltest.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void
 main(int argc, char *argv[])
@@ -6,5 +7,12 @@ main(int argc, char *argv[])
 	helloworld();
 	printint(42);
 	printstring("\nHere is a sample string!", 26);
+	// putchar('Z');
+	char z = 'Z';
+	printstring("\nWriting Z using write:  ", 26);
+	write(1, &z, 1);
+
+	printstring("\nWriting Z using putchar:  ", 28);
+	putchar('Z');
 }
 
